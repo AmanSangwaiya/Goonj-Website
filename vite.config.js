95485cb217aaf75ec92eef203ea61f654e5e2639
AmanSangwaiya <amanjnv455@gmail.com>
2025-12-23 13:21:48 +0530

    make some changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // 👈 THIS IS THE FIX
-  ],
-  base :"/Goonj-Website",
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
